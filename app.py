41e4a05784039ae728271a82cb3a182f23c96f96
Freddie Ergatoudis <freddie.erg@outlook.com>
2020-07-02 14:22:23 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,103 @@
+import random
+
+from flask import Flask, render_template, abort, request, url_for
+from projects import projects as project_list
+import os
+
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+# App Config
+app.url_map.strict_slashes = False  # Allows trailing slashes in URL
+
+# Mail Config
+app.config['MAIL_SERVER'] = 'smtp.zoho.eu'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'admin@emmakirbydesign.co.uk'
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
+
+
+@app.context_processor
+def header_image():
+    def random_image():
+        index_photos = [url_for('static', filename='img/what-we-do/ex2.jpeg'),
+                        url_for('static', filename='img/what-we-do/example.jpeg'),
+                        url_for('static', filename='img/projects/winter-and-summer-barns-with-pool/-WHuAFgq.jpg'),
+                        url_for('static', filename='img/projects/winter-and-summer-barns-with-pool/c8DQ7kLU.jpg'),
+                        url_for('static', filename='img/projects/winter-and-summer-barns-with-pool/5WFU54Oo.jpg'),
+                        url_for('static', filename='img/projects/winter-and-summer-barns-with-pool/eLWg40Q1.jpg'),
+                        url_for('static', filename='img/projects/winter-and-summer-barns-with-pool/pGa9GPQw.jpg'),
+                        url_for('static', filename='img/projects/winter-and-summer-barns-with-pool/y7k8dvwj.jpg'),
+                        url_for('static', filename='img/projects/soho-farmhouse/1Sk3gCOZ.jpeg'),
+                        url_for('static', filename='img/projects/soho-farmhouse/CM2DAZkL.jpeg'),
+                        url_for('static', filename='img/projects/soho-farmhouse/6seWC6EI.jpeg'),
+                        url_for('static', filename='img/projects/soho-farmhouse/uXlZjUMM.jpeg'),
+                        url_for('static', filename='img/projects/village-house-interiors/hjtyCIv0.jpeg'),
+                        url_for('static', filename='img/projects/village-house-interiors/u0xthdMZ.jpeg'),
+                        url_for('static', filename='img/projects/village-house-interiors/JASEW__k.jpeg')]
+        return random.choice(index_photos)
+    return dict(random_image=random_image)
+
+
+@app.route('/')
+def index():
+    return render_template('homepage/index.html')
+
+
+@app.route('/about-us')
+def about_us():
+    return render_template('about-us/about-us.html', title='About Us')
+
+
+@app.route('/what-we-do')
+def what_we_do():
+    return render_template('what-we-do/what-we-do.html', title='What We Do')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects/projects.html', title='Projects', projects=project_list)
+
+
+@app.route('/contact-us', methods=['GET', 'POST'])
+def contact_us():
+    if request.method == 'POST':
+        msg = Message('Service Inquiry', sender=(request.form.get('name'), 'no-reply@emmakirbydesign.co.uk'), recipients=['admin@emmakirbydesign.co.uk'], reply_to=request.form.get('email'))
+        msg.html = request.form.get('message')
+        mail.send(msg)
+        return '1'
+
+    return render_template('contact-us/contact-us.html', title='Contact Us')
+
+
+@app.route('/projects/<project_page>')
+def project(project_page):
+
+    result = None
+    for p in project_list:
+        if p.url == project_page:
+            result = p
+
+    if not result:
+        return abort(404)
+
+    return render_template('project/project.html', project=result, title=result.title)
+
+
+@app.route('/privacy')
+def privacy():
+    return render_template('base/privacy.html', title='Privacy')
+
+
+@app.route('/terms')
+def terms():
+    return render_template('base/terms.html', title='Terms')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ projects.py
@@ -0,0 +1,98 @@
+class Project:
+    def __init__(self, url, title, subtitle, description, thumbnail, images, plans):
+        self.url = url
+        self.title = title
+        self.subtitle = subtitle
+        self.description = description
+        self.thumbnail = 'img/projects/' + thumbnail
+        self.images = images
+        self.plans = plans
+
+
+projects = []
+
+projects.append(
+    Project(url='winter-and-summer-barns-with-pool',
+            title='Winter & Summer Barns With Pool',
+            subtitle='Oxfordshire',
+            description='Working closely with our clients we designed an area which incorporated a new build, '
+                        'traditional oak framed summer barn, swimming pool and a renovated party barn. We '
+                        'reconfigured the internal layout of existing barn to create a new kitchen, wine ‘cellar’ and '
+                        'large double height room housing a bar, dining and seating areas on the ground floor and a '
+                        'guest bedroom and shower room above. \n\nIt is the perfect place for cosy family film nights '
+                        'thanks to a hidden screen or big parties with its long dining table, bar and state of the '
+                        'art sound system. For the warmer months, we designed and built a beautiful building with the '
+                        'help of master craftsmen using traditional methods of oak framing for the structure.\n\nThe '
+                        'plant room, shower, changing rooms sit at one of the barn with an open summer kitchen, '
+                        'bespoke bar with zinc worktop and 5 metre dining table take up the middle section and a '
+                        'cosy, underfloor heated television room means younger children can relax when the sun goes '
+                        'down after a full day swimming. Reclaimed clay tiles and wooden cladding help to give the '
+                        'feel of age to the barn and metal doors to match the replacement doors we put into the party '
+                        'barn unify both buildings.',
+            thumbnail='winter-and-summer-barns-with-pool/thumbnail.jpeg',
+            images=['psyDmPk6.jpg', 'y7k8dvwj.jpg', '-WHuAFgq.jpg', '1eJPZaS1.jpg', '46koBUpT.jpg', '4wRhxyLw.jpg', 'NgB8YejF.jpg', '-UHo4pAd.jpg', '5WFU54Oo.jpg', '5QhAMOdP.jpg', '7zSYs8PQ.jpg', 'eLWg40Q1.jpg', 'xfajmGyU.jpg', 'c8DQ7kLU.jpg', '_cpBQEvY.jpg', 'pGa9GPQw.jpg', 'Qfb4vJSe.jpg', '7h8CKY60.jpg', '0H67D2MP.jpg', 'dUd58Xe9.jpg', 'hoKgJFl4.jpg', 'pBdpjYpN.jpg', 'HPMtX8Ze.jpg', 'ltdBXa9g.jpg', 'A82xgnMn.jpg', 'XctjSj7O.jpg'],
+            plans=[]))
+
+projects.append(
+    Project(url='soho-farmhouse',
+            title='Soho Farmhouse',
+            subtitle='Great Tew, Oxfordshire',
+            description='We loved be part of Farmhouse, a private club nestling in 100 acres of beautiful Cotswold '
+                        'countryside.',
+            thumbnail='soho-farmhouse/thumbnail.jpeg',
+            images=['uXlZjUMM.jpeg', '6seWC6EI.jpeg', 'kPcqLD1Y.jpeg', 'xwgPWcd1.jpeg', '2wmpVdUZ.jpeg', 'jN6x7don.jpeg', '1Sk3gCOZ.jpeg', 'qGQ3R9Qr.jpeg', 'CRJt219v.jpeg', 'M7x2J0NR.jpeg', 'BizpASHX.jpeg', '8TnvWXZU.jpeg', 'CM2DAZkL.jpeg', 'cZUVxI7k.jpeg', 'A3eZfqiG.jpeg', 'FVof2tuB.jpeg', 'gB8DGYeC.jpeg', 'L4p6a2MP.jpeg', 'QRNsiUjo.jpeg'],
+            plans=[]))
+
+projects.append(
+    Project(url='village-house-interiors',
+            title='Village House Interiors',
+            subtitle='Oxfordshire',
+            description='The owners of this beautiful Georgian village house wanted us to redecorate the master and '
+                        'guest bedrooms as well as put in a pool and build a pool house to replace a  rather unloved '
+                        'walled vegetable garden.  New life has been breathed into the formerly neglected area which '
+                        'is now used all summer long. On cooler evenings, meals can be eaten inside the pool house '
+                        'which also has a kitchen and wood burning stove, shower and changing areas making it '
+                        'completely self contained.',
+            thumbnail='village-house-interiors/thumbnail.jpeg',
+            images=['JASEW__k.jpeg', 'zGcHR2Km.jpeg', '7CYh0c5E.jpeg', 'hjtyCIv0.jpeg', 'm_RL2C3h.jpeg', 'SEuT65e1.jpeg', 'ByODXZfw.jpeg', 'wLLhGNoS.jpeg', 'u0xthdMZ.jpeg', '9q8Dzi09.jpeg', '01qY-irj.jpeg', 'A-6cU7NS.jpeg', 'BSVtuhAk.jpeg', 'ilU6hKWU.jpeg'],
+            plans=[]))
+
+projects.append(
+    Project(url='listed-village-house-extension-and-pool',
+            title='Listed Village House Extension and Pool',
+            subtitle='Oxfordshire',
+            description='Opening up a dark, small kitchen and utility room was the remit from the clients. We had '
+                        'already, some years earlier, put built a pool into a walled rose garden. It was a beautiful '
+                        'part of the property but the young family never used it and it felt like a waste of garden. '
+                        'As it was enclosed on all four sides by barns and Cotswold walling, it seemed like the '
+                        'perfect suntrap for the pool. We created a seating area around an outdoor fireplace when the '
+                        'nights turn cooler.\n\nIt was lovely to be invited back to help with the house. The '
+                        'redecoration of sitting room, master bedroom with dressing room and bathroom were first to '
+                        'do followed by the extension design.  A new kitchen with a 4m long island and new Aga was '
+                        'the main focus of the space but also allowed for a seating corner with fireplace, '
+                        'breakfast area, pantry and boot room.',
+            thumbnail='listed-village-house-extension-and-pool.png',
+            images=[],
+            plans=[]))
+
+projects.append(
+    Project(url='contemporary-london-duplex',
+            title='Contemporary London Duplex',
+            subtitle='Primrose Hill, London',
+            description='A single, young professional bought a rundown duplex in Primrose Hill and wanted a light '
+                        'filled, contemporary home. Take a look on our Instagram page for the before and after '
+                        'photos.',
+            thumbnail='contemporary-london-duplex/thumbnail.jpg',
+            images=['yBoPaiMj.jpg', 'mpHZ-GB7.jpg', 'kJqLgZ7L.jpg', 'e74kNaVW.jpg', 'w3ZC50uC.jpg', 'nEe0LlEN.jpg', '3ksgnlmg.jpg', 't-6PyWy3.jpg', 'AvdLVkRS.jpg', 'X80fqLiX.jpg'],
+            plans=[]))
+
+projects.append(
+    Project(url='new-build-village-house',
+            title='New Build Village House',
+            subtitle='Oxfordshire',
+            description='A single, young professional bought a rundown duplex in Primrose Hill and wanted a light '
+                        'filled, contemporary home. Take a look on our Instagram page for the before and after '
+                        'photos.',
+            thumbnail='contemporary-london-duplex/thumbnail.jpg',
+            images=['A-BIUJlM.jpeg', 'UBSYThMa.jpeg', 'bzUM4YwF.jpeg', 'XmATc-cz.jpeg', '6Eqrsszf.jpeg', '8dsOWuxc.jpeg', 'RneVK1V3.jpeg', 'OgPPr7WW.jpeg', 'evYc6YZZ.jpeg', '9MDreQfs.jpeg'],
+            plans=[]))
